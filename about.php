3c01dbb3884573d4d4c993a7a0e00d99c5019716
106208088 <106208088@student.swin.edu.au>
2025-11-14 15:21:59 +0700

    Info update to about.php

--- about.php
+++ about.php
@@ -28,7 +28,7 @@ include_once 'header.inc';
       <ul class="nested">
         <li>Nhat Minh Hoang Vo - Student ID: SWH03431</li>
         <li>Nguyen Minh Quang - Student ID: SWH02960</li>
-        <li>Vu Huu Nhat Minh - Student ID: SWH</li>
+        <li>Vu Huu Nhat Minh - Student ID: SWH03022</li>
       </ul>
 
       <h3>Tutor</h3>
@@ -37,11 +37,11 @@ include_once 'header.inc';
       <h3>Member contributions</h3>
       <dl class="list"> 
         <dt>- Hoang Vo</dt>
-        <dd>Site design, privacy.php, about.php, styles.css</dd>
-        <dt>- Jane Doe</dt>
-        <dd>apply.php, jobs.php, form validation</dd>
-        <dt>- Alex Lee</dt>
-        <dd>index.php images, deployment, styles.css</dd>
+        <dd>Site design, styles.css, enhancements.php, header.inc, footer.inc, nav.inc, privacy.php, management.php, deployment</dd>
+        <dt>- Quang Nguyen</dt>
+        <dd>index.php, about.php, process_eoi.php, settings.php, process_eoi validation</dd>
+        <dt>- Minh Vu</dt>
+        <dd>jobs.php, apply.php, create_tables.sql, implement PHP/MySQL queries</dd>
       </dl>
     </section>
 
@@ -55,7 +55,7 @@ include_once 'header.inc';
         <tbody>
           <tr><td>Hoang Vo</td><td>Networking</td><td>Security, AI</td></tr>
           <tr><td>Quang Nguyen</td><td>Front-end</td><td>Design</td></tr>
-          <tr><td>Alex Lee</td><td>DevOps</td><td>Cloud</td></tr>
+          <tr><td>Minh Vu</td><td>DevOps</td><td>Cloud</td></tr>
         </tbody>
       </table>
     </section>
